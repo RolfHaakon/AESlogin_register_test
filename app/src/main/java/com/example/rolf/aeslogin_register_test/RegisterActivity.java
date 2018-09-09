3e958d4dc8086211b8713662fd82bb786099096a
Rolf <30429371+RolfHaakon@users.noreply.github.com>
2018-09-09 19:10:37 +0800

    Login & Register test w/mySQL

---
+++ app/src/main/java/com/example/rolf/aeslogin_register_test/RegisterActivity.java
@@ -0,0 +1,13 @@
+package com.example.rolf.aeslogin_register_test;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+}
