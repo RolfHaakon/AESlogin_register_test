3663cd6008890e1be0fa6c6919d01dbe025c9895
Rolf <30429371+RolfHaakon@users.noreply.github.com>
2018-09-11 22:48:49 +0800

    -Added page change from login when user click Register. -Added error checking on register page

--- app/src/main/java/com/example/rolf/aeslogin_register_test/MainActivity.java
+++ app/src/main/java/com/example/rolf/aeslogin_register_test/MainActivity.java
@@ -1,13 +1,24 @@
 package com.example.rolf.aeslogin_register_test;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    TextView signup_text;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        signup_text = (TextView) findViewById(R.id.sign_up);
+        signup_text.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this,RegisterActivity.class));
+            }
+        });
     }
 }
